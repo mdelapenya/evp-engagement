b06707266d9e69879f158ab4dd88be12c6508037
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-12-19 12:07:19 -0800

    Create an empty portly for eve

---
+++ plugins-sdk/portlets/evp-portlet/docroot/WEB-INF/src/com/liferay/evp/admin/EVPAdmin.java
@@ -0,0 +1,50 @@
+package com.liferay.evp.admin;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.PortletException;
+import javax.portlet.PortletRequestDispatcher;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * Portlet implementation class EVPAdmin
+ */
+public class EVPAdmin extends LiferayPortlet {
+
+    public void init() {
+        viewTemplate = getInitParameter("view-template");
+    }
+
+    public void doView(
+            RenderRequest renderRequest, RenderResponse renderResponse)
+        throws IOException, PortletException {
+
+        include(viewTemplate, renderRequest, renderResponse);
+    }
+
+    protected void include(
+            String path, RenderRequest renderRequest,
+            RenderResponse renderResponse)
+        throws IOException, PortletException {
+
+        PortletRequestDispatcher portletRequestDispatcher =
+            getPortletContext().getRequestDispatcher(path);
+
+        if (portletRequestDispatcher == null) {
+            _log.error(path + " is not a valid include");
+        }
+        else {
+            portletRequestDispatcher.include(renderRequest, renderResponse);
+        }
+    }
+ 
+    protected String viewTemplate;
+
+    private static Log _log = LogFactoryUtil.getLog(EVPAdmin.class);
+
+}

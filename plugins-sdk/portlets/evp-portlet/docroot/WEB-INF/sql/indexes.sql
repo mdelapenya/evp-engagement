828e86fa383b29c9e3f5742c9059c069f67e6948
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-12-19 13:11:50 -0800

    Revert "Run build-services"
    This reverts commit ed9c39ead5f2b519cacd4d76e6e6786c89903fa3.

--- plugins-sdk/portlets/evp-portlet/docroot/WEB-INF/sql/indexes.sql
+++
@@ -1,6 +0,0 @@
-create index IX_72BF4819 on evp_Fund (projectId);
-
-create index IX_BD7E44A9 on evp_Project (companyId);
-create index IX_E9DFEE49 on evp_Project (managerName);
-
-create index IX_A638905A on evp_Volunteer (groupId);
